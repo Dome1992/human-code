e338288d568522b3e8e6bdad3be04a056a75b2d6
Dome1992 <forlanodomenico92@mail.com>
2023-11-16 15:57:47 +0100

    Lasagne alla Bolognese

---
+++ script.js
@@ -0,0 +1,29 @@
+// LASAGNE ALLA BOLOGNESE 
+
+// START
+
+// Verifica ingrendienti ed utensili
+    // Ci sono gli ingredienti? (Se SI, vai a riga 9; se NO, vai a comprarli e poi vai a riga 9)
+    // Ci sono gli utensili? (Se SI, vai a riga 9; se NO, vai a comprarli e poi vai a riga 9)
+
+// Prepara il sugo alla Bolognese
+    // Cuoci la carne macinara
+    // Aggiungi sedano e carota tritata
+    // Incorpora i pomodori pelati e cuoci lentamente 
+
+// Prepara la besciamella 
+    // Sciogli il burro in una pentola
+    // Aggiungi farina e latte
+
+// Assemblaggio 
+    // Uno strato di besciamella sul fondo della teglia
+    // Pasta per Lasagne
+    // Sugo Bolognese
+    // Ripeti procedimento per N. strati
+    // Concludi con uno strato di besciamella
+
+// Cuoci in forno 
+    // Riscalda il forno
+    // Cuoci fino a doratura
+
+// FINE
